f5e7a24fec8c5fe25375b27202b0cdf608611a01
Luke <lukedeffenbaugh@gmail.com>
2022-12-08 14:05:42 -0600

    Added a constant to allow for pt1 and 2

--- Day 6 - Tuning Trouble/tuning.py
+++ Day 6 - Tuning Trouble/tuning.py
@@ -1,16 +1,18 @@
 
 def main():
+    NUM = 14 # change this to match the number of distinct characters you are looking for
+
     # open and read file, then close
     fp = open("input.txt", "r")
     str = fp.readline()
     fp.close()
 
     # start count at 4, loop until string has less than 4 characters or a break statement is satisfied
-    count = 4
-    while(len(str)>=4):
+    count = NUM
+    while(len(str)>=NUM):
         # save the first 4 characters to new string
         try:
-            newString = str[:4]
+            newString = str[:NUM]
         except:
             break
         
