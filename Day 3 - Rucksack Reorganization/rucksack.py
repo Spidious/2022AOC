15fb8a74dad9fd7010268532c866e87285289b7d
Luke <lukedeffenbaugh@gmail.com>
2022-12-07 21:51:19 -0600

    add comments

--- Day 3 - Rucksack Reorganization/rucksack.py
+++ Day 3 - Rucksack Reorganization/rucksack.py
@@ -6,6 +6,8 @@ def checkupper(char):
         return True
     return False 
 
+# Checks for duplicates in two halves of a string
+# takes two strings, returns integer value of priority for the duplicate
 def checkDuplicates(half1, half2):
     for c in half1:
         for s in half2:
@@ -20,12 +22,13 @@ def main():
     list = []
     count = 0
 
+    # 
     while(True):
         str = f.readline()
         if(str == ''):
             break
         
-        firstpart, secondpart = str[:len(str)//2], str[len(str)//2:]
+        firstpart, secondpart = str[:len(str)//2], str[len(str)//2:]    # I don't know how this works I pulled it off github
         list.append(checkDuplicates(firstpart, secondpart))
         count+=1
     
